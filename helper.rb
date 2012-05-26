533cccfc95f3ffc74c1a30bd8ab30233903e00eb
Sathish <sathish316@gmail.com>
2012-05-26 16:18:57 +0530

    Helper

---
+++ helper.rb
@@ -0,0 +1,5 @@
+helpers do
+  def capitalize(str)
+    str.capitalize
+  end
+end
--- home.rb
+++ home.rb
@@ -2,10 +2,12 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require './config'
+require './helper'
 
 get '/' do
   session[:counter] ||= 0
   session[:counter] += 1
   @counter = session[:counter]
+  @msg = capitalize("hello world")
   erb :index
 end
