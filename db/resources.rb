08edb5cd907695fbed205f00304cf97de9cdc765
Sathish <sathish316@gmail.com>
2012-06-03 03:58:28 +0530

    init debugging

--- db/resources.rb
+++ db/resources.rb
@@ -1,5 +1,10 @@
 def initialize_resources
   Resource.all.each do |resource|
-    resource.init!
+    begin
+      puts "Initializing #{resource.name}"
+      resource.init!
+    rescue => e
+      puts "Failed #{resource.name}: #{e}"
+    end
   end
 end
