8232125dfb13a5c2d5638db92dbbb48dbf0089ce
Sathish <sathish316@gmail.com>
2012-06-03 00:14:19 +0530

    expire content every 1 hour if flag is true

--- models/resource.rb
+++ models/resource.rb
@@ -7,6 +7,8 @@ class Resource
   field :key, type: String
   field :dom_attributes, type: Hash
   field :data, type: Array
+  field :expire_content, type: Boolean
+  field :expire_at, type: DateTime
 
   validates_presence_of :name, :html, :key, :api_path
   validates_uniqueness_of :name, :api_path
@@ -20,7 +22,7 @@ class Resource
   end
 
   def all
-    unless self.data
+    if self.data.nil? or expired?
       logger.info "Parsing html: #{html}"
       self.data = scrapify_klass.all.map {|record| record.attributes.stringify_keys}
       save
@@ -33,6 +35,16 @@ class Resource
   end
 
   private
+
+  def expired?
+    expired = false
+    if expire_content
+      self.expire_at ||= 1.hour.from_now
+      expired = Time.now >= self.expire_at
+      self.expire_at = 1.hour.from_now if expired
+    end
+    expired
+  end
 
   def scrapify_klass
     scrapify_klass_name.constantize
