ac0d9d6d0ae70283c9be251d102aca684ae28da2
Sathish <sathish316@gmail.com>
2012-06-05 20:40:13 +0530

    css selector and xpath selector. css count. TODO: xpath count

--- public/javascripts/preview.js
+++ public/javascripts/preview.js
@@ -1,5 +1,23 @@
 $(function(){
-  // $('.fancybox').fancybox();
+  function logSelectors(element){
+    $(element).contents().find('*').bind('click', function(event){
+      var xpath = $(this).getXPath();
+      xpath = "/" + xpath.join('/');
+      var css = $(this).getCSSPath();
+      console.log('xpath:' + xpath);
+      $('.fancybox-outer .content-selectors .firefly-xpath-selector').val(xpath);
+      console.log('css:' + css);
+      $('.fancybox-outer .content-selectors .firefly-css-selector').val(css);
+      // var xpathCount = document.evaluate('count(' + xpath + ')', element, null, XPathResult.ANY_TYPE, null).numberValue;
+      var cssCount = $(element).contents().find(css).size();
+      // console.log("XPath count:" + xpathCount);
+      // $('#firefly-xpath-count').html(0);
+      console.log("CSS count:" + cssCount);
+      $('.fancybox-outer .content-selectors .firefly-css-count').html(cssCount);
+      event.stopPropagation();
+      event.preventDefault();
+    });
+  }
 
   $('#preview_page').click(function(){
     var url = $('#resource_html').val();
@@ -9,11 +27,14 @@ $(function(){
       console.log('open ' + url);
       $.fancybox({
         href: url,
-        transitionIn: "none",
-        transitionOut: "none",
-        autoScale: true,
-        type: "iframe"
+        autoSize: true,
+        type: "iframe",
+        afterShow: function(){
+          logSelectors($('.fancybox-inner iframe'));
+          $('.fancybox-inner iframe').contents().find('body').find('.apify_overview').click(function(){alert('overview')});
+        }
       });
+      $('.fancybox-inner iframe').before($('.content-selectors').clone().show());
     }
   });
 });
