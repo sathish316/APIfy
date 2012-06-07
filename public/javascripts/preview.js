0e3e49432dcbab76905efdf92561a406a18e6789
Sathish <sathish316@gmail.com>
2012-06-08 01:36:05 +0530

    xpath highlight working

--- public/javascripts/preview.js
+++ public/javascripts/preview.js
@@ -1,5 +1,6 @@
 $(function(){
   function logSelectors(element){
+    $(element).contents().find('*').unbind('click')
     $(element).contents().find('*').bind('click', function(event){
       // XPath selector
       var xpath = $(this).getXPath();
@@ -14,8 +15,10 @@ $(function(){
       $('.fancybox-outer .content-selectors .firefly-css-selector').val(css);
       updateCssCount();
 
+      highlight();
       event.stopPropagation();
-      event.preventDefault();
+      // event.preventDefault();
+      return false;
     });
   }
 
@@ -33,24 +36,58 @@ $(function(){
     var cssCount = $(iframe).contents().find(css).size();
     console.log("CSS count:" + cssCount);
     $('.fancybox-outer .content-selectors .firefly-css-count').html(cssCount);
-    highlightSelectedElements();
   }
 
-  function highlightSelectedElements(){
-    var css = $('.fancybox-outer .content-selectors .firefly-css-selector').val();
+  function highlight(){
+    addStyles();
+    highlightXPath();
+    highlightCss();
+  }
+
+  function addStyles(){
     var iframe = $('.fancybox-inner iframe');
-    if($('#firefly-styles').size() == 0){
-      var highlightStyle = '<style type="text/css" id="firefly-styles">.firefly-highlight { background-color:#99FFCC;}</style>';
-      $(highlightStyle).appendTo($(iframe).contents().find('head'));
+    $(iframe).contents().find('head').find('#firefly-xpath-styles').remove();
+    // var style = '<style type="text/css" id="firefly-xpath-styles">.firefly-xpath-highlight { background-color: #57FFF4;}</style>';
+    var style = '<style type="text/css" id="firefly-xpath-styles">.firefly-xpath-highlight { outline: 2px solid #FF0000 !important;}</style>';
+    $(style).appendTo($(iframe).contents().find('head'));
+    $(iframe).contents().find('head').find('#firefly-css-styles').remove();
+    var style = '<style type="text/css" id="firefly-css-styles">.firefly-css-highlight { background-color: #99FFCC;}</style>';
+    $(style).appendTo($(iframe).contents().find('head'));
+  }
+
+  function highlightCss(){
+    var iframe = $('.fancybox-inner iframe');
+    var css = $('.fancybox-outer .content-selectors .firefly-css-selector').val();
+    $(iframe).contents().find('.firefly-css-highlight').removeClass('firefly-css-highlight');
+    $(iframe).contents().find(css).addClass('firefly-css-highlight');
+  }
+
+  function highlightXPath(){
+    var iframe = $('.fancybox-inner iframe');
+    var iframeDoc = window.frames[0].document;
+    var xpath = $('.fancybox-outer .content-selectors .firefly-xpath-selector').val();
+    $(iframe).contents().find('.firefly-xpath-highlight').removeClass('firefly-xpath-highlight');
+    var nodes = [];
+    try {  
+      var iterator = iframeDoc.evaluate(xpath, iframeDoc, null, XPathResult.ANY_TYPE, null);
+      var node = iterator.iterateNext();
+      while (node) {
+        nodes.push(node);
+        node = iterator.iterateNext();  
+      }   
+    } catch (e) {  
+      console.log('Error: Document tree modified during iteration ' + e);  
+    } 
+    for(i=0; i<nodes.length; i++){
+      nodes[i].className = nodes[i].className + ' firefly-xpath-highlight';
     }
-    $(iframe).contents().find('.firefly-highlight').removeClass('firefly-highlight');
-    $(iframe).contents().find(css).addClass('firefly-highlight');
   }
 
   function bindSelectorToUpdateCount(){
     $('.fancybox-outer .content-selectors .firefly-xpath-selector').keypress(function(event){
       if(event.keyCode == 13){
         updateXPathCount();
+        highlightXPath();
         return false;
       } else {
         return true;
@@ -59,6 +96,7 @@ $(function(){
     $('.fancybox-outer .content-selectors .firefly-css-selector').keypress(function(event){
       if(event.keyCode == 13){
         updateCssCount();
+        highlightCss();
         return false;
       } else {
         return true;
