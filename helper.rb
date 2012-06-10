ccc95daa9f2bbfcf0b09d9db369f50ba7651cd14
Sathish <sathish316@gmail.com>
2012-06-10 19:23:28 +0530

    converting app from Sinatra classic to Sinatra::Base. Pagination using Kaminari and Padrino helpers

--- helper.rb
+++ helper.rb
@@ -1,5 +1,9 @@
-helpers do
-  def capitalize(str)
-    str.capitalize
+class Apify < Sinatra::Base
+  helpers do
+    def capitalize(str)
+      str.capitalize
+    end
   end
+  register ::Kaminari::Helpers::SinatraHelpers
+  register ::Sinatra::Partial
 end
--- preview.rb
+++ preview.rb
@@ -1,7 +1,10 @@
-require 'sinatra'
+require 'sinatra/base'
 
-get '/preview' do
-  puts "Preview: #{params[:url]}"  
-  uri = URI(params[:url])
-  Net::HTTP.get_response(uri).body
+class Apify < Sinatra::Base
+
+  get '/preview' do
+    puts "Preview: #{params[:url]}"  
+    uri = URI(params[:url])
+    Net::HTTP.get_response(uri).body
+  end
 end
