edd2a58bddb7e4f80c62d59498d6d2b586df8cb2
Sathish <sathish316@gmail.com>
2012-06-11 10:00:46 +0530

    search nop on enter

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,18 +1,27 @@
-var searchInProgress = false;
-$('#search-api').typeahead({
-  source: function (typeahead, query) {
-    if ($('#search-api').val().length > 2 && !searchInProgress){
-      mixpanel.track('Search');
-      searchInProgress = true;
-      return $.get('/api_search.json', { query: query }, function (data) {
-        return typeahead.process(data);
-      }).complete(function(){
-        searchInProgress = false;
-      });
+$(function(){
+  var searchInProgress = false;
+  $('#search-api').typeahead({
+    source: function (typeahead, query) {
+      if ($('#search-api').val().length > 2 && !searchInProgress){
+        mixpanel.track('Search');
+        searchInProgress = true;
+        return $.get('/api_search.json', { query: query }, function (data) {
+          return typeahead.process(data);
+        }).complete(function(){
+          searchInProgress = false;
+        });
+      }
+    },
+    property: 'name',
+    onselect: function(api){
+      window.location = api.url;
     }
-  },
-  property: 'name',
-  onselect: function(api){
-    window.location = api.url;
-  }
-});
+  });
+
+  $('#search-api').keypress(function(event){
+    if(event.keyCode == 13){
+      return false;
+    }
+  });
+
+})
